e3703eee2f33237a3a84098f0f9229bf4fe1e834
yayayahei <yayayahei@outlook.com>
2022-04-14 20:25:57 +0800

    add function

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
+    printf("%d", add(1, 2));
     return 0;
 }
